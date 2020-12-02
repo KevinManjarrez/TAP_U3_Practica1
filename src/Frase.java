d993ee49ef3c59a199bac42c86bde36c3a1e436f
Kevin Manjarrez <keazmanjarrezma@ittepic.edu.mx>
2020-12-01 23:41:43 -0700

    Versión 1

--- src/Frase.java
+++ src/Frase.java
@@ -2,16 +2,6 @@
 import java.awt.Color;
 import java.awt.Graphics2D;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author pc
- */
 public class Frase {
     int x;
     int y;
@@ -20,6 +10,29 @@ public class Frase {
     int desplazamientoY;
     
     
+    public void pintar(Graphics2D g2, String s){
+        int i = (int)(Math.random()*39);
+        g2.setColor(new Color((int)(Math.random()*250),(int)(Math.random()*255),(int)(Math.random()*255)));
+        g2.drawString(s, x, y);
+        
+    }
+    
+    
+    public void rebotar(){
+        x += desplazamientoX;
+        y += desplazamientoY;
+        
+        if(x <= 0){
+            desplazamientoX = 5;
+        }
+        if(x >= 600){
+            desplazamientoX = -5; 
+        }
+        
+        if(y <= 0 || y >= 400){
+            desplazamientoY *= -1;
+        }
+    }
     
     public Frase(){
         x = (int)(Math.random()*600+10);
@@ -38,38 +51,6 @@ public class Frase {
             desplazamientoY = 5;
         }else{
             desplazamientoY = -5;
-        }
-    }
-    
-    public void pintar(Graphics2D g2, String s){
-        int i = (int)(Math.random()*39);
-        g2.setColor(new Color((int)(Math.random()*250),(int)(Math.random()*255),(int)(Math.random()*255)));
-        g2.drawString(s, x, y);
-        
-    }
-    
-    
-    public void rebotar(){
-        //1 respecto a X  2 margenes  0 izq   800 der
-        //2 respecto a Y  2 margenes  0 arriba  600 abajo
-        
-        //desplazamientoX valga NEGATIVO cuando x toque 800 y POSITIVO cuando llegue a 0
-        //desplazamientoY valga NEGATIVO cuando y toque 800 y POSITIVO cuando llegue a 0
-        
-        x += desplazamientoX;
-        y += desplazamientoY;
-        
-        //analisis de rebote para x
-        if(x <= 0){
-            desplazamientoX = 5;
-        }
-        if(x >= 600){
-            desplazamientoX = -5; 
-        }
-        
-        //analisis de rebote para y
-        if(y <= 0 || y >= 400){
-            desplazamientoY *= -1;
         }
     }
 }
--- src/Frases.java
+++ src/Frases.java
@@ -7,26 +7,71 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.Timer;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author pc
- */
 public class Frases extends Canvas{
     Frase f = new Frase();
     
     Timer timer,timer2;
     int i;
-    String[] frases = {"Hola mundo","Programación es lo mejor","TAP la mejor materia","POO sirve de mucho","Estructura de Datos","Java es un enguaje","NetBeans es un IDE","Eclipse es un IDE","Python buen lenguaje","SQL para base de datos",
-                       "Hilos tercera unidad","Canvas es para graficos","Thread es hilo","Javascript lenguaje","Ingeniería es lo mejor","Google gran empresa","FaceBook red social","Amazon para compras","Netflix ver series","Sillicon Valley",
-                       "Developer","Tester","Sistemas Computacionales","BackEnd es bueno","FrontEnd es diseño","FullStack","Base de Datos","18401120 mi No. Control","Juan Diego mi nombre",
-                       "Machine Learning","Inteligencia Artificial","Block Chain","Computer Science","Código para crear cosas","StackOverflow sirve de mucho",
-                       "Codecademy para aprender","Web Development","Android es lo mejor","Apple de IOS"};
+    String[] frases = 
+    {"Nuestras vidas se definen por las oportunidades, incluso las que perdemos",
+     "Lo que importa verdaderamente en la vida no son los objetivos que nos marcamos,\n"
+      + " sino los caminos que seguimos para lograrlo",
+     "El hombre que más ha vivido no es aquel que más años ha cumplido,\n"
+     + "sino aquel que más ha experimentado la vida",
+     "Si lo puedes soñar, lo puedes hacer",
+     "Somos lo que hacemos de forma repetida. Por tanto, la excelencia no es un acto, sino un hábito",
+     "Lo imposible es el fantasma de los tímidos y el refugio de los cobardes",
+     "Comienza a verte a como un alma con un cuerpo en lugar de un cuerpo con un alma",
+     "Vale más actuar exponiéndose a arrepentirse de ello, que arrepentirse de no haber hecho nada",
+     "El camino que nos toca recorrer está lleno de sorpresas. \n"
+      + "Nunca vas a estar preparado para las que te toquen a ti,\n "
+      + "sean dichosas o sombrías, pues eso es parte de adquirir experiencia.\n "
+      + "Y descubrir cuán gratas o desafortunadas son las que te esperan, es algo nunca podrás eva",
+     "Una emoción no causa dolor. La resistencia o supresión de una emoción causa dolor",        
+     "La felicidad no es algo que pospones para el futuro, es algo que diseñas para el presente",
+     "Todos quieren la paz, y para asegurarla, fabrican más armas que nunca",
+     "Todo el mundo piensa en cambiar el mundo, pero nadie piensa en cambiarse a sí mismo",
+     "Si exagerásemos nuestras alegrías, como hacemos con nuestras penas, nuestros problemas perderían importancia",
+     "El amigo ha de ser como el dinero, que antes de necesitarle, se sabe el valor que tiene",
+     "Tus acciones serán el reflejo de la manera que tienes de ver la vida y las que te van definir ante\n"
+      + " los demás. No las malgastes en cosas y actitudes que no valen la pena, solo tú puedes decidir\n"
+      + " la forma en la que quieres que te recuerden, porque no estarás en este mundo para siempre",
+     "Quién puede decir cuánto ama, pequeño amor siente",
+     " El amor es lo que mueve al mundo aunque a veces parezca lo contrario.\n"
+     + " A las personas no nos haría mal recordar esto de vez en cuando",
+     "La ciencia es el gran antídoto contra el veneno del entusiasmo y la superstición",
+     "Nunca terminas de conocer a la gente. Tus amigos, te familia y hasta tú mismo,\n "
+      + "pueden ocultar sorpresas que en la vida te podrías haber imaginado, tanto buenas como malas",          
+     "Todos tenemos el mismo destino, en esencia no hay manera de diferenciarnos si nacemos\n"
+      + " para llorar y reír. Recuérdalo, todos tenemos los días contados,\n "
+      + "vive cada uno de tus días como si fueran el regalo más grande, porque nadie\n "
+      + "puede asegurarte el mañana",
+     "Todos deseamos llegar a viejos; y todos negamos que hemos llegado",
+     "Mientras vivas vas a encontrarte con todo tipo de personas, tanto buenas como malas.\n"
+      + "Es imposible adivinar las intenciones que oculta alguien detrás de su comportamiento,\n"
+      + " pero descubrirlo es la tarea más interesante y peligrosa con la que te puedes llegar a topar",
+     "La decepción después de un amor fallido, puede llegar a oprimir tu corazón hasta\n"
+     + " el punto de no dejarte respirar. Pero nadie se ha muerto de amor",
+     "Si quieres cambiar las frutas, primero tendrás que cambiar las raíces.\n"
+     + "Si quieres cambiar lo visible, primero debes cambiar lo invisible",
+     "No llores por la gente que se ha ido, enfócate en quienes se encuentran a tu lado\n"
+     + " en el presente y quédate con los buenas recuerdos de los que se marcharon",
+     "No hay deber que descuidemos tanto como el deber de ser felices",
+     "¿Quieres ser rico? Pues no te afanes es aumentar tus bienes, sino en disminuir tu codicia",
+     "No debes enfocarte en el dolor que puedes sentir si alguien te ha falla.\n"
+      + "Si no eres capaz de perdonar una equivocación, entiérrala y sigue adelante",            
+     "No dejes de tener fe en los demás, por una o varias personas que te hayan decepcionado",
+     " ¡Ojalá vivas todos los días de tu vida!",
+     "Amar es la aventura más grande en la que te puedes embarcar. Porque te puede hacer volar más\n"
+      + " alto de lo que jamás imaginarte y también ponerte los pies de la tierra",
+     "Es difícil confiar después de haber recibido un golpe bajo, pero allá afuera hay gente\n"
+      + " de buen corazón que puede compensar tus desilusiones. No hagas a un lado la oportunidad de conocerla",
+     "Un corazón alegre vive más tiempo",
+     "Solos podemos hacer poco, juntos podemos hacer mucho", 
+     "La medida de la salud no es estar adaptado a una sociedad profundamente enferma",
+     "Qué maravilloso es que nadie necesita un solo momento antes de comenzar a mejorar el mundo",
+     "Siempre son las personas a las que más amamos, las que nos pueden hacer mayor daño",
+     "La vida es un 10% lo que me ocurre y 90% cómo reacciono a ello"};
     
     public Frases(){
      
